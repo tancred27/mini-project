9d8770fa45b92b4668de31387864ed45e58026ad
Vardhan <mobpsycho2736@gmail.com>
2021-07-06 20:01:49 +0530

    deployment to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
+const path = require("path");
 const config = require("config");
 const PORT = config.get("PORT") || 4000;
 const connectDB = require("./config/db");
@@ -45,6 +46,13 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/college", require("./routes/college"));
 app.use("/api/user", require("./routes/user"));
 app.use("/api/cdn", require("./routes/cdn"));
+
+// Serve static assets in production:
+if (process.env.NODE_ENV === "production") {
+    // Set static folder:
+    app.use(express.static("/client/build"));
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")));
+}
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
